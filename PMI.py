65a0542c89bd354b38836329585057faa8fc08be
Dixita Patel <dixita42@yahoo.com>
2018-10-11 17:02:01 -0700

    Update PMI.py

--- PMI.py
+++ PMI.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import nltk
 from nltk.collocations import *
 from nltk.corpus import stopwords
 
-
-# In[ ]:
-
-
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 trigram_measures = nltk.collocations.TrigramAssocMeasures()
-
-
-# In[ ]:
 
 
 data = []
@@ -28,51 +18,16 @@ data
 text = ''.join(data)
 tokens = nltk.wordpunct_tokenize(text)
 
-
-# In[ ]:
-
-
 finder = BigramCollocationFinder.from_words(tokens)
 
-
-# In[ ]:
-
-
 finder.apply_freq_filter(2) 
-
-
-# In[ ]:
-
 
 #finding collocations words
 #extract n-grams from your data and then find the ones that have the highest point wise mutual information (PMI)
 #Collocations are expressions of multiple words which commonly co-occur
 finder.nbest(bigram_measures.pmi, 20) 
 
-
-# In[ ]:
-
-
 #spanning intervening words:
 ignored_words = nltk.corpus.stopwords.words('english')
 finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
 finder.nbest(bigram_measures.likelihood_ratio, 20)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
